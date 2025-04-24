25e95753f3e18f5f22ad292913d9b4ac5ec15b07
Sergio Sant'Anna <serginhocls@gmail.com>
2025-04-24 18:45:14 +0100

    Função remoção
    Feita a função de remoção

--- Estoque.cpp
+++ Estoque.cpp
@@ -9,26 +9,25 @@
 using namespace std;
 
 
-//Estrutura do produto
+//Estrutura do produto.
 struct Produto {
     int id;
     string nome;
     int quantidade;
     float precoCusto;
 };
-
-//função que diz ao cout como mostrar item por item
+//função que diz ao cout como mostrar item por item.
 ostream& operator<<(ostream& os, const Produto& p) {
     os << "ID: " << p.id << " | "
         << "Nome: " << p.nome << " | "
         << "Quantidade: " << p.quantidade << " | "
-        << "Preco: €" << p.precoCusto << endl;
+        << "Preco: " << p.precoCusto << endl;
     return os;
 }
 
-//Insere produtos no estoque
+//Insere produtos no estoque.
 Produto estoque[] = {
-    {1, "Placa Mãe ASUS B550", 20, 85.00},
+    {1, "Placa Mae ASUS B550", 20, 85.00},
     {2, "Processador Ryzen 5 5600X", 20, 140.00},
     {3, "Memoria RAM 16GB DDR4", 20, 35.00},
     {4, "Disco SSD 1TB NVMe", 20, 55.00},
@@ -50,107 +49,199 @@ Produto estoque[] = {
     {20, "Base Refrigerada Notebook", 20, 14.00}
 };
 
-//Calcula o tamanho do estoque para o usar o ciclo de repetição 
+//Calcula o tamanho do estoque para o usar o ciclo de repetição.
 const int tamEstoque = sizeof(estoque) / sizeof(estoque[0]);
 
 //função mostrar estoque
 void mostrarEstoque() {
-    cout << "------ PRODUTOS DISPONIVEIS ------\n";
     for (int i = 0; i < tamEstoque; i++) {
         cout << estoque[i];
     }
 }
-//Função para escolher os produtos e a quantidade
-void selecionarProduto() {
-    int qtdProdutoVenda, idProduto, qtdVenda;
-    float valorPago;
-
-    cout << "Inserir a quantidade de produtos desejados: ";
-    cin >> qtdProdutoVenda;
-    for (int i = 0; i < qtdProdutoVenda; i++) {
-        cout << "Digite o id do produto que deseja: ";
-        cin >> idProduto;
-        cout << "Digite a quantidade do produto que deseja: ";
-        cin >> qtdVenda;
-        for (int j = 0; j < tamEstoque; j++) {
-
+//função para verificar se o produto escolhido tem no estoque
+void checarProdutoEstoque(int idProduto, Produto*& produtoSelecionado) {
+    //percorre o estoque e se encontrar o produto no estoque, guarda o vetor no ponteiro.
+    produtoSelecionado = nullptr; // Inicia o ponteiro a nullo, para não haver lixo
+    for (int i = 0; i < tamEstoque; i++) {
+        if (estoque[i].id == idProduto) {
+            produtoSelecionado = &estoque[i];
+            break;
         }
-
-
     }
-
 }
 
-// O menu que vai fazer a gestão do stock
-void menuGestao() {
-    int opcaoUser;
-
-    // Menu com opções de admin/gerenciamento de artigos
-    do {
-        system("cls");
-        cout << "------ GESTÃO DE STOCK ------" << endl;
-        cout << "1. Adicionar artigo\n";
-        cout << "2. Remover produto\n";
-        cout << "9. Sair\n";
-        cout << endl << ": ";
-
-        cin >> opcaoUser;
-
-        switch (opcaoUser)
-        {
-        case 1:
-            // Menu adicionar artigos
-            break;
-        case 2:
-            // Menu de remoção
-            break;
-        case 9:
-            break;
-        default:
-            cout << "Opção invalida, selecione novamente.\n";
-            break;
-        }
-    } while (opcaoUser != 9);
+// Função para gerar venda aleatória grátis a cada 4 compras
+int vendaGratis() {
+    return rand() % 4 == 0;
 }
 
-// Menu onde o usuario vai fazer compra
-void compraMenu() {
-    int opcao = 0;
+// Função para adicionar 30% ao valor de custo
+//float calcValorVenda(float precoCusto) {
+//    return precoCusto * 1.3;
+//}
 
-    do {
-        system("cls");
+// Função para calular o iva
+//float calcIVA(float precoTotal) {
+//    return precoTotal * 0.23;
+//}
 
-        mostrarEstoque();
-        cout << endl << endl << ": ";
-        cin >> opcao;
-    } while (opcao != 9);
+void removerProduto() {
+    // Mostra o estoque pra o utilizador ver o que quer remover
+    mostrarEstoque();
+    int idProduto;  // Id do produto que vai ser removido
+
+    cout << "Insira o produto: ";
+    cin >> idProduto;
+
+    Produto* produtoSelecionado = &estoque[idProduto];
+    for (int i = 0; i < tamEstoque; i++) {
+        cout << "Produto removido";
+        checarProdutoEstoque(idProduto, produtoSelecionado);
+        produtoSelecionado = &estoque[idProduto - 1];
+        produtoSelecionado->quantidade = 0;
+        break;
+    }
 }
+
+
+//Função para escolher os produtos e a quantidade.
+// void venda() {
+//     int qtdProdutoVenda, idProduto, qtdVenda, numCliente;
+//     static int numFatura = 1;
+//     float valorPago, IVA, precoTotal, precoUnit, troco, totalComIVA=0;
+//     //coloca o apontador do produto selecionado a nulo.
+//     Produto* produtoSelecionado = nullptr; // Já declarado fora do loop
+//
+//     mostrarEstoque();
+//
+//     cout << "Inserir a quantidade de produtos desejados: ";
+//     cin >> qtdProdutoVenda;
+//
+//     //iniciar Matriz
+//     float mat[qtdProdutoVenda][5];
+//
+//     for (int i = 0; i < qtdProdutoVenda; i++) {
+//         cout << "Digite o id do produto: ";
+//         cin >> idProduto;
+//         checarProdutoEstoque(idProduto, produtoSelecionado);
+//         produtoSelecionado = &estoque[idProduto-1];
+//         cout << "Digite a quantidade de " << produtoSelecionado->nome << " que deseja: ";
+//         cin >> qtdVenda;
+//
+//         //caso o id digitado não esteja na lista, informa que não foi encontrado.
+//         if (!produtoSelecionado) {
+//             cout << "Produto nao encontrado.\n";
+//             return;
+//         }
+//         //caso a quantidade vendida seja maior que o estoque, informa que não tem disponibilidade.
+//         if (qtdVenda > produtoSelecionado->quantidade) {
+//             cout << "Quantidade em stock insuficiente.\n";
+//             qtdProdutoVenda--;
+//             mostrarEstoque();
+//             continue;
+//         }
+//
+//         precoUnit = calcValorVenda(produtoSelecionado->precoCusto);
+//         precoTotal = precoUnit * qtdVenda;
+//         IVA = calcIVA(precoTotal);
+//         totalComIVA = precoTotal + IVA;
+//
+//         mat[i][0] = idProduto;
+//         mat[i][1] = qtdVenda;
+//         mat[i][2] = precoTotal;
+//         mat[i][3] = IVA;
+//         mat[i][4] = totalComIVA;
+//
+//     //diminui os produtos vendidos do estoque
+//     produtoSelecionado -> quantidade -= qtdVenda;
+//
+//     mostrarEstoque();
+//     }
+//
+//     //verifica se a venda sorteada é verdadeira e coloca o Total = 0
+//     bool vendaSorteada = vendaGratis();
+//     if (vendaSorteada) {
+//         totalComIVA = 0;
+//         IVA = 0;
+//         cout << "Informe ao cliente que a venda dele foi sorteada e será gratis.\n";
+//     }
+//
+//     cout << "Digite o Numero do cliente: ";
+//     cin >> numCliente;
+//
+//     //calcula o valor total usando um operador ternário
+//     float somaTotal = 0;
+//     for (int i = 0; i < qtdProdutoVenda; ++i) {
+//         somaTotal += vendaSorteada ? 0 : mat[i][4];
+//     }
+//     //Verifica se a venda foi sorteada, caso não informa o valor total e pede o valor pago pelo cliente para calcular o troco
+//     if (!vendaSorteada) {
+//         cout << "Total a pagar: " << somaTotal << " euros\n";
+//         cout << "Inserir o valor pago: ";
+//         cin >> valorPago;
+//     } else {
+//         valorPago = 0;
+//     }
+//
+//     troco = valorPago - somaTotal;
+//
+//     //imprir talão
+//     time_t t = time(nullptr);
+//     tm* dataAtual = localtime(&t);
+//
+//     cout << "\n======= TALAO DE COMPRA =======\n";
+//     cout << "Fatura N: " << numFatura++ << endl;
+//     cout << "Data: " << put_time(dataAtual, "%d/%m/%Y %H:%M") << endl;
+//     cout << "Cliente N: " << numCliente << endl;
+//     for (int i = 0; i < qtdProdutoVenda; i++) {
+//         int produtoTalao = static_cast<int>(mat[i][0]);
+//         Produto* produtoTalao = nullptr;
+//         if (produtoTalao) {
+//             cout << "Produto: " << produtoTalao->nome << endl;
+//             cout << "Preço Unitário: " << calcValorVenda(produtoTalao->precoCusto) << " euros" << endl;
+//             cout << "Quantidade: " << static_cast<int>(mat[i][1]) << endl;
+//             cout << "Preço s/IVA do produto: " << mat[i][2] << " euros" << endl;
+//             cout << "IVA (23%): " << mat[i][3] << " euros" << endl;
+//         }
+//     }
+//     cout << "Total c/IVA: " << somaTotal << " euros" << endl;
+//     cout << "Valor Entregue: " << valorPago << " euros" << endl;
+//     cout << "Troco: " << troco << " euros" << endl;
+//     cout << "===============================\n";
+// }
+
 
 //função menu
 void exibirMenu() {
     int opcao;
     //menu com as interações do programa
-
     do {
-        system("cls");
-        cout << "------MENU PRINCIPAL ------" << endl;
+        cout << "------MENU PRINCIPAL------" << endl;
         cout << "1. Efetuar Venda" << endl;
-        cout << "2. Gestão de stock" << endl;
-        cout << "9. Sair" << endl;
+        cout << "2. Criar Novo Artigo" << endl;
+        cout << "3. Excluir Produto" << endl;
+        cout << "4. Exibir Stock" << endl;
+        cout << "5. Sair" << endl;
         cout << "Escolha uma opcao: ";
         cin >> opcao;
 
         switch (opcao) {
         case 1:
             // chamar função de venda aqui
-            compraMenu();
+            // venda();
             break;
         case 2:
-            // chamar função de gestão
-            menuGestao();
+            // chamar função de criação
             break;
-        case 9:
-            system("cls");
+        case 3:
+            // chamar função de eliminação
+            removerProduto();
+            break;
+        case 4:
+            // chamar função mostrar estoque
+            mostrarEstoque();
+            break;
+        case 5:
             cout << "Saindo do programa..." << endl;
             break;
         default:
@@ -159,16 +250,11 @@ void exibirMenu() {
 
         cout << endl;
 
-    } while (opcao != 9);
+    } while (opcao != 5);
 }
 
-
 int main() {
-    // Muda a linguagem do programa pra português, permitindo o uso de caracteres especiais
     setlocale(LC_ALL, "Portuguese");
-    // Faz os valores aparecerem com duas casas decimais
-    cout << fixed << setprecision(2);
 
-    // Chama a função de inicio
     exibirMenu();
 }
